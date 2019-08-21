1680b5d3ce3342aad71e4835ae57be2b04a25299
Aaron Davis <adbe455@gmail.com>
2019-08-21 13:45:50 -0400

    character info

--- api.js
+++ api.js
@@ -2,9 +2,6 @@ const search = document.getElementById('search');
 const searchBtn = document.getElementById('button-addon2');
 const resultBox = document.getElementById('result-box')
 
-let i = 0;
-let searchType;
-let infoType;
 let resultsFound;
 let peopleBtn = [];
 // let locationsBtn = [];
@@ -15,10 +12,10 @@ searchBtn.addEventListener('click', runSearch);
 
 // Click the search button when 'Enter' is pressed in the search bar
 search.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    searchBtn.click();
-  }
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchBtn.click();
+    }
 });
 
 function runSearch() {
@@ -38,20 +35,20 @@ function runSearch() {
 }
 
 function fetchResults(type) {
-    searchType = type
     fetch('https://ghibliapi.herokuapp.com/films').then(function(result) {
     return result.json();
-    }).then(function(json) {
-    checkResults(json);
-    });
+}).then(function(json) {
+    checkResults(json, type);
+});
 
-    resultsFound = 0;
-    i = 0;
+resultsFound = 0;
+i = 0;
 }
 
-function checkResults(json){
+let i = 0;
+function checkResults(json, type){
     console.log(json)
-    if(searchType == 'title') {
+    if(type == 'title') {
         for(; i < json.length; i++) {
             if(json[i].title.toLowerCase().includes(search.value.toLowerCase())) {
                 console.log('Title match:', i, json[i].title);
@@ -60,7 +57,7 @@ function checkResults(json){
             }
         }
     }
-    else if(searchType == 'year'){
+    else if(type == 'year'){
         for(; i < json.length; i++) {
             if(json[i].release_date == search.value) {
                 console.log('Year match:', i, json[i].title, json[i].release_date);
@@ -90,7 +87,7 @@ function displayResults(film) {
     let col1 = document.createElement('div'); // film art
     let col2 = document.createElement('div'); // title + info
     let col3 = document.createElement('div'); // description
-    let col4 = document.createElement('div');
+    // let col4 = document.createElement('div');
     let image = document.createElement('img');
     let title = document.createElement('h3');
     let description = document.createElement('p')
@@ -106,11 +103,13 @@ function displayResults(film) {
     // let speciesBtn = document.createElement('button');
     // let vehiclesBtn = document.createElement('button');
     
-    row.className = "row result";
-    col1.className = "c col1 col-sm-2";
-    col2.className = "c col2 col-sm-3";
-    col3.className = "c col3 col-sm-5";
-    col4.className = "c col4 col-sm-2";
+    row.className = "row result row" + i;
+    col1.className = "c col1 col-lg-2 col-sm-auto";
+    col2.className = "c col2 col-lg-4 col-sm-auto";
+    col3.className = "c col3 col-lg-6 col-sm-auto";
+    // col4.className = "c col4 col-sm-2";
+
+    peopleBtn.setAttribute("data-toggle", "button");
 
     peopleBtn.type = "button";
     // locationsBtn.type = "button";
@@ -123,7 +122,7 @@ function displayResults(film) {
     // vehiclesBtn.classList = "btn btn-outline-light vehicles";
 
     
-    peopleBtn.textContent = "People";
+    peopleBtn.textContent = "Characters ››";
     // locationsBtn.textContent = "Locations";
     // speciesBtn.textContent = "Species";
     // vehiclesBtn.textContent = "Vehicles";
@@ -134,6 +133,7 @@ function displayResults(film) {
     
     
     image.src = './assets/film-art/' + i + '.jpg';
+    image.style = "display: block";
     title.textContent = film.title;
     description.textContent = film.description;
     director.textContent = 'Director: ' + film.director;
@@ -147,11 +147,12 @@ function displayResults(film) {
     // if(film.locations[0].includes('-')) {btnGroup.appendChild(locationsBtn)}
     // if(film.species[0].includes('-')) {btnGroup.appendChild(speciesBtn)}
     // if(film.vehicles[0].includes('-')) {btnGroup.appendChild(vehiclesBtn)}
+    btnGroup.appendChild(peopleBtn);
     
     row.appendChild(col1);
     row.appendChild(col2);
     row.appendChild(col3);
-    row.appendChild(col4);
+    // row.appendChild(col4);
     
     col1.appendChild(image);
     col2.appendChild(title);
@@ -160,7 +161,7 @@ function displayResults(film) {
     col2.appendChild(producer);
     col2.appendChild(rtScore);
     col3.appendChild(description);
-    col4.appendChild(btnGroup);
+    if(true){col3.appendChild(btnGroup)};
     
     resultBox.appendChild(row);
     resultBox.appendChild(br);
@@ -178,14 +179,137 @@ function displayResults(film) {
     // speciesBtn.id = species;
     // vehiclesBtn.id = vehicles;
     
-    // document.getElementById().addEventListener('click', () => fetchInfo("people", film.id))
+    let rowNum = "row" + i
+    document.getElementById(people).addEventListener('click', () => fetchInfo("people", film.id, rowNum))
 }
 
-function fetchInfo(type, id) {
-    // infoType = type;
-    fetch('https://ghibliapi.herokuapp.com/' + type).then(function(result) {
-    return result.json();
-    }).then(function(json) {
-    checkResults(json);
-    });
+function fetchInfo(type, id, rowNum) {
+    // let table = document.getElementsByClassName("table")[0];
+    let row = document.getElementsByClassName(rowNum)[0];
+    if(!row.childNodes[3]){
+        // removes previous tables
+        // if(table){
+        //     table.parentElement.removeChild(table);
+        // }
+        fetch('https://ghibliapi.herokuapp.com/' + type).then(function(result) {
+        return result.json();
+        }).then(function(json) {
+        displayInfo(json, type, id, rowNum);
+        });
+    } else {
+        row.removeChild(row.childNodes[3]);
+    }
+}
+
+function displayInfo(json, type, id, rowNum) {
+    if(type === "people") {
+        let peopleArray = [];
+        for(let j = 0; j < json.length; j++) {
+            // if(json[j].films.includes(id)) {
+            //     peopleArray.push(json[j])
+            // }
+            for(let k = 0; k < json[j].films.length; k++) {
+                if(json[j].films[k].includes(id)) {
+                    peopleArray.push(json[j]);
+                }
+            }
+        }
+        console.log(peopleArray);
+        if(peopleArray.length > 0) {
+            // let row = document.createElement('div');
+            // row.className = "row"
+
+
+            let table = document.createElement('table');
+            let thead = document.createElement('thead');
+            let tr1 = document.createElement('tr');
+            let tbody = document.createElement('tbody');
+            let thName = document.createElement('th');
+            let thGender = document.createElement('th');
+            let thAge = document.createElement('th');
+            let thEyeColor = document.createElement('th');
+            let thHairColor = document.createElement('th');
+
+            table.className = "table";
+
+            thName.textContent = "Name";
+            thGender.textContent = "Gender";
+            thAge.textContent = "Age";
+            thEyeColor.textContent = "Eye Color";
+            thHairColor.textContent = "Hair Color";
+
+            thName.scope = "col";
+            thGender.scope = "col";
+            thAge.scope = "col";
+            thEyeColor.scope = "col";
+            thHairColor.scope = "col";
+
+            table.appendChild(thead);
+            thead.appendChild(tr1);
+            tr1.appendChild(thName);
+            tr1.appendChild(thGender);
+            tr1.appendChild(thAge);
+            tr1.appendChild(thEyeColor);
+            tr1.appendChild(thHairColor);
+            table.appendChild(tbody);
+            document.getElementsByClassName(rowNum)[0].appendChild(table);
+
+            for(e of peopleArray){
+                let tr = document.createElement('tr');
+                let tdName = document.createElement('td');
+                let tdGender = document.createElement('td');
+                let tdAge = document.createElement('td');
+                let tdEyeColor = document.createElement('td');
+                let tdHairColor = document.createElement('td');
+
+                tdName.textContent = e.name;
+                tdGender.textContent = e.gender;
+                tdAge.textContent = e.age;
+                tdEyeColor.textContent =  e.eye_color;
+                tdHairColor.textContent = e.hair_color;
+
+                tbody.appendChild(tr);
+                tr.appendChild(tdName);
+                tr.appendChild(tdGender);
+                tr.appendChild(tdAge);
+                tr.appendChild(tdEyeColor);
+                tr.appendChild(tdHairColor);
+                
+                
+                // let col = document.createElement('div');
+                // col.className = "col-sm-3 infocol";
+                
+                // let name = document.createElement('h5');
+                // name.textContent = e.name;
+                
+                // let gender = document.createElement('p');
+                // gender.textContent = "Gender: " + e.gender;
+                
+                // let age = document.createElement('p');
+                // age.textContent = "Age: " + e.age;
+                
+                // let eyeColor = document.createElement('p');
+                // eyeColor.textContent = "Eye color: " + e.eye_color;
+                
+                // let hairColor = document.createElement('p');
+                // hairColor.textContent = "Hair color: " + e.hair_color;
+                
+                // let br = document.createElement('br');
+                
+                // row.appendChild(col);
+                // col.appendChild(name);
+                // col.appendChild(br);
+                // col.appendChild(gender);
+                // col.appendChild(age);
+                // col.appendChild(eyeColor);
+                // col.appendChild(hairColor);
+                // document.getElementsByClassName(rowNum)[0].appendChild(row);
+            }
+        } else {
+            let para = document.createElement('p');
+            para.textContent = "No results found.";
+            para.className = "col-sm-12 text-center";
+            document.getElementsByClassName(rowNum)[0].appendChild(para);
+        }
+    }
 }
